62a82fe4a4ccb0b033eda5b3d96935e795a67a70
prahaai <prahaai@gmail.com>
2009-05-09 22:03:59 +0000

    Major speed updates. Switch from Python array.array to numpy.ndarray. Data no longer saved as compressed string ; saved as list of ndarray. Improved yaml dump speed by creating custom representer/ constructor, which compress decoded ndarray.dumps(). Tested one Cython version, but i think i'm doing it wrong, because it's slower than Python+Psyco version. TODO : must further test and improve Spawn.

--- app/Example_LetterMonster_Consume.py
+++ app/Example_LetterMonster_Consume.py
@@ -4,34 +4,34 @@ sys.path.insert( 0, os.getcwd() )
 from _letter_monster import LetterMonster
 
 #
-
 lm = LetterMonster()
 lm.DEBUG = True
-
-#lm.Consume( image='..\\Behind_Your_Illusion_by_littlemewhatever.jpg', text='Example_Consume.txt', x=0, y=175, pattern='sharp', filter='detail' )
-#lm.Consume( image='..\\Chimpance__by_faboarts.jpg', text='Example_Consume.txt', x=0, y=175, pattern='smooth', filter='blur' )
-#lm.Consume( image='..\\Robot_chicken_by_net_surfer.jpg', text='Example_Consume.txt', x=0, y=175, pattern='cro', filter='smooth_more' )
-#lm.Consume( image='..\\Sunstone_by_nmsmith.jpg', text='Example_Consume.txt', x=0, y=175, pattern='letters', filter='smooth|detail|smooth|sharpen' )
-#lm.Consume( image='..\\Waiting_for_Godot_by_humanskin.jpg', text='Example_Consume.txt', x=0, y=175, pattern='numbers', filter='smooth' )
-#lm.Consume( image='..\\Wind_by_pincel3d.jpg', text='Example_Consume.txt', x=0, y=175, pattern='dos', filter='smooth' )
-
-#lm.Consume( image='..\\So_fragile_____by_Prahaai.jpg', text='Example_Consume.txt', x=0, y=175, pattern='sharp', filter='detail' )
-#lm.Consume( image='..\\Ana_by_Prahaai.jpg', text='Example_Consume.txt', x=0, y=175, pattern='sharp', filter='smooth|detail' )
-#lm.Consume( image='..\\garbov_by_Arukasme.jpg', text='Example_Consume.txt', x=0, y=175, pattern='standard', filter='sharp|smooth' )
-
-lm.Consume( image='../Moon.jpg', x=0, y=0, pattern='dos' )
-
-try: os.remove( 'test_lmgl_c.yaml' )
+#
+lm.Consume( image='../Wallpaper.jpg', x=0, y=0, pattern='default' )
+#
+try: os.remove( 'test_lmgl_cons.yaml' )
 except: pass
-lm.Save( 'test_lmgl_c.yaml' )
-
+lm.Save( 'test_lmgl_cons.yaml' )
 #
-
+print 'Body 1: ', lm.body, '\n'
+#
+print 'Data is type:', type(lm.body['raster1'].data)
+print 'Inside Data, is type:', type(lm.body['raster1'].data[0])
+print 'Data is len:', len(lm.body['raster1'].data)
+print 'Data 0-10:', lm.body['raster1'].data[0][0:10]
+#
+lm.Load( 'test_lmgl_cons.yaml' )
+del lm.body['raster1'].data[0]
+#
+print 'Body 2: ', lm.body, '\n'
+#
+print 'Data is type:', type(lm.body['raster1'].data)
+print 'Inside Data, is type:', type(lm.body['raster1'].data[0])
+print 'Data is len:', len(lm.body['raster1'].data)
+print 'Data 0-10:', lm.body['raster1'].data[0][0:10]
+#
 print( "Please, use Lucida Console Bold, size 4 for viewing." )
-
 #
-
 print( 'Finished.\n' )
 os.system( 'pause' )
-
 #
