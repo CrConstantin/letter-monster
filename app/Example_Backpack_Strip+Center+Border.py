38aa61518607ba40978715ddcab04fb1eaa48b2f
prahaai <prahaai@gmail.com>
2009-05-12 10:50:21 +0000

    Fixed last version bug in Spawn. Was not overwriting correctly. Changed and tested all examples. Changed test LMGL file and test file for backpack example. Added function in Backpack for transforming text into YAML dump.

--- app/Example_Backpack_Strip+Center+Border.py
+++ app/Example_Backpack_Strip+Center+Border.py
@@ -1,7 +1,7 @@
 
 import os, sys
 sys.path.insert( 0, os.getcwd() )
-from _letter_monster import Backpack
+from _classes import Backpack
 
 #
 
@@ -9,22 +9,28 @@ vInput = open( 'To_Test.txt', 'r' )
 vOutput = open( 'Example_Strip+Center+Border.txt', 'w' )
 
 _bp = Backpack()
+print( 'Starting to read example file...\n' )
 vResult = vInput.read()
 
-vResult = _bp.Rotate90Right( vResult )
+
+vResult = _bp.Rotate90Right( vResult ) # Rotate, then undo.
 vResult = _bp.Rotate90Left( vResult )
-vResult = _bp.Reverse( vResult )
+
+vResult = _bp.Reverse( vResult ) # Reverse, then undo.
 vResult = _bp.Reverse( vResult )
 
-vResult = _bp.AlignRight( vResult )
+vResult = _bp.AlignRight( vResult ) # Align right and left.
 vResult = _bp.AlignLeft( vResult )
 
-vResult = _bp.StripLeftSpace( vResult )
+vResult = _bp.StripLeftSpace( vResult ) # Strip spaces.
 vResult = _bp.StripRightSpace( vResult )
 
-vResult = _bp.Center( vResult )
-vResult = _bp.Border( vResult )
+vResult = _bp.Center( vResult ) # Center.
+vResult = _bp.Border( vResult ) # Border.
+vResult = _bp.RightBorder( vResult, Char='~' )
+vResult = _bp.LeftBorder( vResult, Char='~' )
 vResult = _bp.Border( vResult, Char='#', Thick=2 )
+
 
 vOutput.write( vResult )
 
--- app/Example_LetterMonster_Spawn.py
+++ app/Example_LetterMonster_Spawn.py
@@ -4,12 +4,16 @@ sys.path.insert( 0, os.getcwd() )
 from _letter_monster import LetterMonster
 
 #
+
 lm = LetterMonster()
 lm.DEBUG = True
+
+print( 'Spawning...\n' )
+lm.Spawn( 'test_lmgl_cons_BIG.yaml', 'txt' )
+
 #
-print( 'Spawning...' )
-lm.Spawn( 'test_lmgl_cons.yaml', 'txt' )
-#
+
 print( 'Finished.\n' )
 os.system( 'pause' )
+
 #
