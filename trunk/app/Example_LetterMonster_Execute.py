3ca78dd275d52499bec0426b0ac91f793be3de26
prahaai@gmail.com <prahaai@gmail.com@08971430-33e5-11de-94d6-134fa40061eb>
2009-05-15 12:05:35 +0000

    Added Export as CSV and HTML. Fixed small possible bug in Spawn.

--- trunk/app/Example_LetterMonster_Execute.py
+++ trunk/app/Example_LetterMonster_Execute.py
@@ -8,7 +8,7 @@ from _letter_monster import LetterMonster
 lm = LetterMonster()
 lm.DEBUG = True
 
-lm.Load( 'test_lmgl_cons_BIG.yaml' )
+lm.Load( 'test_lmgl.yaml' )
 print 'Executing vector instructions...\n'
 print 'Instructions are', lm.body['vect1'].instructions
 print 'Instructions are type:', type(lm.body['vect1'].instructions)
