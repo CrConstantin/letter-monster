a0082af42b2153cfcbdc8d4a217eed4e54650be9
prahaai@gmail.com <prahaai@gmail.com@08971430-33e5-11de-94d6-134fa40061eb>
2009-05-15 08:13:21 +0000

    Moved Patterns into __init__ function. Deleted Feed and Command functions. Will probably implement them later on... Implemented Spit function to render LetterMonster in WINDOWS COMMAND PROMPT.

---
+++ trunk/app/Example_LetterMonster_Spit.py
@@ -0,0 +1,21 @@
+
+import os, sys
+sys.path.insert( 0, os.getcwd() )
+from _letter_monster import LetterMonster
+
+#
+
+lm = LetterMonster()
+lm.DEBUG = True
+
+lm.Load( 'test_lmgl_cons.yaml' )
+print
+
+lm.Spit('WIN CMD')
+print
+
+#
+
+os.system( 'pause' )
+
+#
