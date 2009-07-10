f6d40af6a0d72c3e73a6f820b2c2fcd4840c86d2
prahaai@gmail.com <prahaai@gmail.com@08971430-33e5-11de-94d6-134fa40061eb>
2009-07-10 08:01:46 +0000

    Small changes.

--- trunk/app/Animations/Bug.py
+++ trunk/app/Animations/Bug.py
@@ -8,11 +8,11 @@ The original image is called "bug.gif". I don't know who is the author, i'm sorr
 '''
 
 import os, sys
-sys.path.insert( 0, os.getcwd().replace('Examples','')[:-1] )
+sys.path.insert( 0, os.getcwd().replace('Animations','')[:-1] )
 from _letter_monster import LetterMonster
 
 lm = LetterMonster()
 lm.DEBUG = True
 lm.Number_Of_Threads = 1
 lm.Load( 'Bug.lmgl' ) # Change LMGL file here.
-lm.Render( 'pygame', size=(250,510) )
+lm.Render( 'pygame', size=(250,510), txtcolor=(0, 0, 0), bgcolor=(100, 155, 133) )
